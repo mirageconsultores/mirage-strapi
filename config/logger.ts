9f47959dff929b51a7edeca66ef8e364e4a21986
mauriciolaser <mauriciocastrovaldez@gmail.com>
2025-07-02 19:13:39 +0300

    logger

--- config/logger.ts
+++ config/logger.ts
@@ -1,8 +1,7 @@
 // config/logger.ts
-import { StrapiLoggerConfig } from '@strapi/strapi';
-
-export default ({ env }): StrapiLoggerConfig => ({
+export default ({ env }: { env: (key: string, defaultValue?: string) => string }) => ({
+  // Nivel 'silly' para tener todos los logs: silly, debug, info, warn, error
   level: env('STRAPI_LOG_LEVEL', 'silly'),
-  // Opcional: muestra la pila de errores en consola
+  // Muestra la pila de errores completa en consola
   exposeInContext: true,
 });
