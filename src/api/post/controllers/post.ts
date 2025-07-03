73e00fa419bdc76623dc9ac91e48d8dc43c63f6e
mauriciolaser <mauriciocastrovaldez@gmail.com>
2025-07-03 10:40:31 +0300

    modificado controller post

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -2,6 +2,27 @@
  * post controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
+import type { Context } from 'koa';
 
-export default factories.createCoreController('api::post.post');
+export default factories.createCoreController('api::post.post', ({ strapi }) => ({
+  async find(ctx: Context) {
+    // Sólo poblamos la relación 'imagen'
+    ctx.query = {
+      ...ctx.query,
+      populate: { imagen: true },
+    };
+    const { data, meta } = await super.find(ctx);
+    return { data, meta };
+  },
+
+  async findOne(ctx: Context) {
+    // Igual para findOne
+    ctx.query = {
+      ...ctx.query,
+      populate: { imagen: true },
+    };
+    const { data, meta } = await super.findOne(ctx);
+    return { data, meta };
+  },
+}));
